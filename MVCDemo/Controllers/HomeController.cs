321c18ceb7ea2166f0dff9b6a1c1225bb7922bff
srinivasnikhil <46591903+srinivasnikhil@users.noreply.github.com>
2021-02-09 14:12:35 +0530

    Update HomeController.cs

--- MVCDemo/Controllers/HomeController.cs
+++ MVCDemo/Controllers/HomeController.cs
@@ -34,6 +34,7 @@ namespace MVCDemo.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            //Error Action
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
